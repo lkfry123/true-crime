8e9163b7d1b75f30f69c696337ead1343b6b3e2d
Laura Tinio <laurara1013@gmail.com>
2025-10-12 22:06:04 -0700

    e

--- true-crime/astro.config.mjs
+++ true-crime/astro.config.mjs
@@ -3,7 +3,7 @@ import sitemap from '@astrojs/sitemap';
 import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://edgein.netlify.app',
   trailingSlash: 'always',
   output: 'static',
   integrations: [sitemap()],
